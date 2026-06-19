d13eabc514918f15ace6b221c940ff35fcf238f3
LindoM-Labs <makanda.lindokuhle@gmail.com>
2026-06-19 13:44:47 +0200

    Product to Cart Confirmation Test Complete

--- tests/ClientAppLoginPage.spec.js
+++ tests/ClientAppLoginPage.spec.js
@@ -43,14 +43,14 @@ test('Invalid Password Test', async({page}) =>
     await expect(page.locator('.toast-message')).toHaveText('Incorrect email or password.');
     page.pause();
 
-    // const invalidCredentialError = await page.locator('.toast-message').textContent();
-    // console.log(invalidCredentialError);
+
 });
 
 test('End to End Test', async({page}) =>
 {
+    
     await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
-   
+   //Login with valid credentials
     const emailField = page.getByPlaceholder('email@example.com');
     const passwordField = page.locator('#userPassword');
     const loginBtn = page.locator('#login');
@@ -58,19 +58,25 @@ test('End to End Test', async({page}) =>
     await emailField.fill('makanda.odwa@gmail.com');
     await passwordField.fill('Issie24!');
     await loginBtn.click();
-    
-  
+    //Login successful assertion
      const text = await page.locator('.toast-success').textContent();
      expect(text?.trim()).toBe('Login Successfully');
      console.log(text);
-
+    //Adding a product to the cart
     await page.getByRole('heading', {name: 'Automation'}).waitFor();
     const myProduct = await page.locator('h5').nth(2).textContent(); // Getting the name of the product i will be placing in the cart and use it for validations
     await page.locator("[class*='w-10']").nth(2).click(); // Clicking on the add to cart button of the product i will be placing in the cart
     await page.locator("[class*='ng-trigger']").waitFor();
     const productToCartConfirmationText = await page.locator("[class*='toast-success']").textContent();
-    console.log(productToCartConfirmationText);
+    //Added to cart assertion
     expect(productToCartConfirmationText?.trim()).toBe("Product Added To Cart");
+    console.log(productToCartConfirmationText);
+    //Going to the cart page
+    const cartBtn = await page.locator("[class*='btn-custom']").nth(2);
+    await cartBtn.click();
+    await page.getByRole('heading', {name: 'My Cart'}).waitFor();
+    const cartItems = await page.locator('.cart h3');
+    await expect(cartItems).toContainText(myProduct);// .toContainText() is used to go through the text elements especially if they are a collection of elements and check if the text is present in any of them. .toHaveText() is used to check if the text is present in a single element
 
   
 });
